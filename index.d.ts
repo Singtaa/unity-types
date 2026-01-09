ec93f0e2d8eb15076415b59d49b23338f4229059
Singtaa <126031603+Singtaa@users.noreply.github.com>
2026-01-08 21:47:14 -0800

    Clean up index.d.ts to use triple-slash references

--- index.d.ts
+++ index.d.ts
@@ -2,33 +2,22 @@
 // https://github.com/Singtaa/unity-types
 //
 // Usage:
-//   npm install unity-types@~6000.3.0
+//   npm install unity-types
 //
 // Add to tsconfig.json:
-//   { "compilerOptions": { "types": ["unity-types"], "skipLibCheck": true } }
+//   { "compilerOptions": { "types": ["unity-types"] } }
+//
+// Then use CS.* types directly:
+//   const go = new CS.UnityEngine.GameObject("MyObject")
+//   const vec = new CS.UnityEngine.Vector3(1, 2, 3)
 
-// Helper types for C# interop
-declare interface $Ref<T> { __doNotAccess: T }
-declare interface $Out<T> { __doNotAccess: T }
-declare interface $Task<T> { __doNotAccess: T }
-
-// Type incompatibility marker (prevents accidental type assignment)
-declare namespace CS {
-    const __keep_incompatibility: symbol;
-}
-
-// Base .NET types
-/// <reference path="./_system.d.ts" />
-
-// Unity modules
-/// <reference path="./UnityEngine.CoreModule.d.ts" />
-/// <reference path="./UnityEngine.UIElementsModule.d.ts" />
-/// <reference path="./UnityEngine.InputLegacyModule.d.ts" />
-/// <reference path="./Unity.InputSystem.d.ts" />
-/// <reference path="./UnityEngine.PhysicsModule.d.ts" />
-/// <reference path="./UnityEngine.Physics2DModule.d.ts" />
-/// <reference path="./UnityEngine.AudioModule.d.ts" />
-/// <reference path="./UnityEngine.UnityWebRequestModule.d.ts" />
-
-// OneJS types
-/// <reference path="./OneJS.d.ts" />
+/// <reference path="_system.d.ts" />
+/// <reference path="UnityEngine.CoreModule.d.ts" />
+/// <reference path="UnityEngine.UIElementsModule.d.ts" />
+/// <reference path="UnityEngine.PhysicsModule.d.ts" />
+/// <reference path="UnityEngine.Physics2DModule.d.ts" />
+/// <reference path="UnityEngine.AudioModule.d.ts" />
+/// <reference path="UnityEngine.InputLegacyModule.d.ts" />
+/// <reference path="UnityEngine.UnityWebRequestModule.d.ts" />
+/// <reference path="Unity.InputSystem.d.ts" />
+/// <reference path="OneJS.d.ts" />
